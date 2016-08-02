be15e9a88971239471cb1d0a09ce1ab4c0d25b83
El Cattivo <elcattivo@byteland.cc>
2016-08-02 21:23:48 +0200

    Fix time of execution

--- AdAttack.meta.js
+++ AdAttack.meta.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name            AdAttack
 // @namespace       https://github.com/elcattivo/AdAttack
-// @version         0.1.0-alpha
+// @version         0.1.1-alpha
 // @description     Removes ads shown by AdDefend.
 // @author          elcattivo
 // @copyright       2016, El Cattivo (http://byteland.cc)
 // @license         The MIT License (MIT); https://opensource.org/licenses/MIT
 // @include         *
 // @grant           none
-// @run-at          document-start
 // @nocompat        Chrome
 // @updateURL       https://raw.githubusercontent.com/elcattivo/AdAttack/master/AdAttack.meta.js
 // @downloadURL     https://raw.githubusercontent.com/elcattivo/AdAttack/master/AdAttack.user.js
